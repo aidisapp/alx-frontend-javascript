63298f3493af6875a74c4744240e28f60ba9480b
Idongesit Ekanem <aidisapp94@gmail.com>
2024-10-01 16:11:41 +0100

    Update the file to make it pass lint

---
+++ 0x02-ES6_classes/eslint.config.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    files: ['**/*.js'], // Lint all JS files
+    rules: {
+      'no-unused-vars': 'warn',
+      'semi': ['error', 'always'], // Example rules
+      // Add other rules here
+    },
+  },
+];
