09afe0235a4877b9e89b0a460ef8a995b4f24c55
Idongesit Ekanem <aidisapp94@gmail.com>
2024-10-30 19:35:07 +0100

    I starting sorted the error issues with this task

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,14 +1,14 @@
 import Car from './10-car.js';
 
-export default class EVCar extends Car {
+class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  // This is the eslint-disable-next-line class-methods-use-this
   cloneCar() {
-    const Clone = super.constructor[Symbol.species];
-    return new Clone();
+    return new Car(this._brand, this._motor, this._color);
   }
-}  
+}
+
+export default EVCar;
