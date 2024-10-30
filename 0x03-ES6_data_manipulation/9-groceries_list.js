11b6404f18c1d696976fd93920c36123c7d0d02b
Idongesit Ekanem <aidisapp94@gmail.com>
2024-10-30 13:32:01 +0100

    I still had to fix some errors

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,11 @@
-export default function groceriesList() {
-  const shoppingList = new Map([
-    ['Apples', 10],
-    ['Tomatoes', 10],
-    ['Pasts', 1],
-    ['Rice', 1],
-    ['Banana', 5],
-  ]);
-  return shoppingList;
-}
+const groceriesList = () => {
+  const map = new Map();
+  map.set('Apples', 10);
+  map.set('Tomatoes', 10);
+  map.set('Pasta', 1);
+  map.set('Rice', 1);
+  map.set('Banana', 5);
+  return map;
+};
+
+export default groceriesList;
